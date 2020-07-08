6468a8f1c6d1594d901394dada0271446bc4979e
J.Hu <jhu@apigarage.com>
2020-07-08 17:44:48 -0400

    update apollo client curl example

--- src/App.js
+++ src/App.js
@@ -1,155 +1,265 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import './App.css';
+import ApolloClient from 'apollo-boost';
+import { gql } from 'apollo-boost';
+import { useQuery, useMutation, useLazyQuery } from '@apollo/react-hooks';
+import { ApolloProvider } from '@apollo/react-hooks';
+import useInput from './hooks/useInput';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
 
-////////// Context Example
+///// Apollo Client
 
-// const { useContext, useState, createContext } = React
-// const AppContext = createContext()
-
-// function useAppContext() {
-//   const context = useContext(AppContext)
-//   if (!context)
-//     throw new Error('useAppContext must be used within AppProvider!')
-//   return context
-// }
-
-// function AppProvider(props) {
-//   // the count for our counter component
-//   const [count, setCount] = useState(0)
-//   // this message never changes!
-//   const [message, setMessage] = useState('Hello from Context!')
-//   const value = {
-//     count,
-//     setCount,
-//     message,
-//     setMessage
-//   }
-//   return <AppContext.Provider value={value} {...props} />
-// }
-
-// function Message() {
-//   const { message } = useAppContext()
-//   // the text will render to a random color for
-//   // each instance of the Message component
-//   const getColor = () => (Math.floor(Math.random() * 255))
-//   const style = {
-//     color: `rgb(${getColor()},${getColor()},${getColor()})`
-//   }
-//   return (
-//     <div>
-//       <h4 style={style}>{message}</h4>
-//     </div>
-//   )
-// }
-
-// function Count() {
-//   const { count, setCount } = useAppContext()
-//   return (
-//     <div>
-//       <h3>Current count from context: {count}</h3>
-//       <button onClick={() => setCount(count + 1)}>Increment</button>
-//     </div>
-//   )
-// }
-
-// function App() {
-//   return (
-//     <div>
-//       <AppProvider>
-//         <h2>Re-renders! 😩</h2>
-//         <Message />
-//         <Message />
-//         <Message />
-//         <Count />
-//       </AppProvider>
-//     </div>
-//   )
-// }
-
-////////// Redux Hook Example Peer Component
-
-// function Message() {
-//   const message = useSelector(state => state.counter.message);
-//   // the text will render to a random color for
-//   // each instance of the Message component
-//   const getColor = () => (Math.floor(Math.random() * 255))
-//   const style = {
-//     color: `rgb(${getColor()},${getColor()},${getColor()})`
-//   }
-//   return (
-//     <div>
-//       <h4 style={style}>{message}</h4>
-//     </div>
-//   )
-// }
-
-// function Count() {
-//   const dispatch = useDispatch();
-//   const count = useSelector(state => state.counter.counter);
-//   return (
-//     <div>
-//       <h3>Current count from context: {count}</h3>
-//       <button onClick={() => dispatch({ type: 'INCREMENT' })}>Increment</button>
-//     </div>
-//   )
-// }
-
-// function App() {
-//   return (
-//     <div>
-//       <h2>Re-renders! 😩</h2>
-//       <Message />
-//       <Message />
-//       <Message />
-//       <Count />
-//     </div>
-//   )
-// }
-
-
-
-////////// Redux Hook Example Child Component Re-render 
-
-function Message() {
-  const message = useSelector(state => state.counter.message);
-  // the text will render to a random color for
-  // each instance of the Message component
-  const getColor = () => (Math.floor(Math.random() * 255))
-  const style = {
-    color: `rgb(${getColor()},${getColor()},${getColor()})`
+const GET_POST = gql`
+  query getPost($postId: Int!) {
+    getPost(id:$postId) {
+      id
+      title
+      category{
+        id
+        name
+      }
   }
+}
+`;
+
+const UPDATE_POST = gql`
+mutation updatePost($id: Int!, $categoryId: Int!, $title: String!, $content: String!) {
+  updatePost(id: $id, categoryId: $categoryId, title: $title, content: $content){
+      id
+      title,
+      content,
+      category{
+        id,
+        name
+      }
+    }
+}
+`;
+
+const CREATE_POST = gql`
+  mutation createPost($categoryId: Int!, $title: String!, $content: String!) {
+    createPost(categoryId: $categoryId, title: $title, content: $content){
+      id
+      title
+      category{
+        id
+        name
+      }
+    }
+  }
+`;
+
+const GET_ALL_POSTS = gql`
+  query getAllPosts {
+      getAllPost {
+        id
+        title
+        category{
+          id
+          name
+        }
+    }
+  }
+`;
+
+const DELETE_POST = gql`
+  mutation deletePost($id: Int!) {
+    deletePost(id: $id) {
+      id
+    }
+  }
+`;
+
+
+function GetPost() {
+  const [postId, setPostId, binding] = useInput();
+  const [getPost, { loading, data, error }] = useLazyQuery(GET_POST);
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    getPost({
+      variables: {
+        postId: parseInt(postId)
+      }
+    })
+  }
+
   return (
     <div>
-      <h4 style={style}>{message}</h4>
+      <h1>Get Post >>>>>>></h1>
+      <input {...binding} value={postId} placeholder='Id' >
+      </input>
+      <button onClick={handleSubmit}> Search </button>
+      {
+        loading && <p> loading... </p>
+      }
+      {
+        error && <p>error: {error.message}</p>
+      }
+      {
+        data && 
+        <p> id: <b>{data.getPost.id}</b> title: <b>{data.getPost.title}</b> category: <b>{data.getPost.category.map(item => item.name)}</b></p>
+      }
+    </div>
+  )
+}
+function SignlePost({postId}) {
+  const { loading, error, data, networkStatus } = useQuery(GET_POST, 
+    {
+      variables: {
+        postId
+      },
+      // pollInterval: 2000,
+      // skip: !postId,
+      notifyOnNetworkStatusChange: true,
+
+    });
+
+  if (networkStatus === 4) return 'refetching!';
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+
+  return (
+    <div>
+      <h1>Post Content >>>>>>></h1>
+      <p> id: <b>{data.getPost.id}</b> title: <b>{data.getPost.title}</b> category: <b>{data.getPost.category.map(item => item.name)}</b></p>
     </div>
   )
 }
 
-// changed to use React.memo
-const MemoizedMessage = React.memo(Message)
-
-function Count() {
-  const dispatch = useDispatch();
-  const count = useSelector(state => state.counter.counter);
+function UpdatePost() {
+  let input;
+  const [updatePost, { data, called }] = useMutation(UPDATE_POST);
   return (
     <div>
-      <h3>Current count from context: {count}</h3>
-      <button onClick={() => dispatch({ type: 'INCREMENT' })}>Increment</button>
-      <MemoizedMessage />
+      <h1>Update Post >>>>>>>>>>>>>></h1>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          updatePost({ variables: { id: 13, categoryId: 1, title: input.value, content: '123' } });
+          input.value = '';
+        }}
+      >
+        <input
+        placeholder='title'
+          ref={node => {
+            input = node;
+          }}
+        />
+        <button type="submit">Update Post</button>
+      </form>
+    </div>
+  );
+}
+
+function AddPost() {
+  let input;
+  // CREATE_POST query should return the same fields as GET_ALL_POSTS
+  const [createPost] = useMutation(CREATE_POST, {
+    update(cache, { data: { createPost } }) {
+      const { getAllPost } = cache.readQuery({ query: GET_ALL_POSTS });
+      cache.writeQuery({
+        query: GET_ALL_POSTS,
+        data: { getAllPost: getAllPost.concat([createPost]) },
+      });
+    }
+  });
+  return (
+    <div>
+      <h1>Add Post >>>>>>>>>>>>>>>>>.</h1>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          createPost({ variables: { categoryId: 1, title: input.value, content: '123' } });
+          input.value = '';
+        }}
+      >
+        <input
+          placeholder='title'
+          ref={node => {
+            input = node;
+          }}
+        />
+        <button type="submit">Add Post</button>
+      </form>
+    </div>
+  );
+}
+
+function AllPosts() {
+  const { loading, error, data, networkStatus } = useQuery(GET_ALL_POSTS,
+    {
+      notifyOnNetworkStatusChange: true
+    }
+  );
+
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+
+  return (
+    <div>
+      <h1>All Posts: >>>>>>></h1>
+      {
+        data.getAllPost.map(post => {
+          return (
+            <div>
+              <p> id: <b>{post.id}</b> title: <b>{post.title}</b> category: <b>{post.category.map(item => item.name)}</b></p>
+            </div>
+          );
+        })
+      }
     </div>
   )
+}
+
+function DeletePost() {
+  let input;
+  const [deletePost] = useMutation(DELETE_POST, {
+    update(cache, { data: { deletePost } }) {
+      const { getAllPost } = cache.readQuery({ query: GET_ALL_POSTS });
+      cache.writeQuery({
+        query: GET_ALL_POSTS,
+        data: { getAllPost: getAllPost.filter(post => post.id != deletePost.id) },
+      });
+    }
+  });
+  return (
+    <div>
+      <h1>Delete Post >>>>>>>>>>>>>>>>>.</h1>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          deletePost({ variables: { id: parseInt(input.value) } });
+          input.value = '';
+        }}
+      >
+        <input
+          placeholder='Id'
+          ref={node => {
+            input = node;
+          }}
+        />
+        <button type="submit">Delete Post</button>
+      </form>
+    </div>
+  );
 }
 
 function App() {
   return (
-    <div>
-      <h2>Re-renders! 😩</h2>
-      <Message />
-      <Message />
-      <Count />
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <GetPost/>
+        <UpdatePost/>
+        <AddPost/>
+        <DeletePost/>
+        <AllPosts/>
+      </div>
+    </ApolloProvider>
   )
 }
 
